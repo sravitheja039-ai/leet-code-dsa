6a458034a6aefcbb7bcf62bf2ab0cf61a700f6d0
RAVITHEJA S <sravitheja039@gmail.com>
2026-07-20 09:30:58 +0530

    Time: 633 ms (59.58%), Space: 22.3 MB (53.7%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,39 +1,39 @@
 class Solution:
-    def threeSum(self, nums):
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
+        n = len(nums)
 
-        for i in range(len(nums) - 2):
+        for i in range(n - 2):
 
-            if nums[i] > 0:
-                break
-
+            # Skip duplicate first elements
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            left = i + 1
-            right = len(nums) - 1
+            j = i + 1
+            k = n - 1
 
-            while left < right:
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
 
-                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    j += 1
 
-                if total == 0:
-                    ans.append([nums[i], nums[left], nums[right]])
-
-                    left += 1
-                    right -= 1
-
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif total < 0:
-                    left += 1
+                elif total > 0:
+                    k -= 1
 
                 else:
-                    right -= 1
+                    ans.append([nums[i], nums[j], nums[k]])
+
+                    j += 1
+                    k -= 1
+
+                    # Skip duplicate second elements
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+                    # Skip duplicate third elements
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
 
         return ans
